ef86fadb89d4624e8a63ed1c34184695cfda79f8
eprado99 <a01570318@itesm.mx>
2023-05-11 09:38:19 -0600

    add hide check, styling to active header and current date

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,25 +11,34 @@ interface HeaderProps {
 export function Header({items}: HeaderProps) {
     const pathname = usePathname();
     return (
-        <header className={"mt-4"}>
+        <header className={"mt-4 mb-3"}>
             <nav>
-            {
-                
-                items.map((item, index) => {
-                    const isActive = pathname.startsWith(item.href)
+                {
                     
-                    return (
-                    <Link
-                        key={index}
-                        href={item.disabled ? "#" : item.href}
-                        className={`text-gray-900 border-transparent flex-1 whitespace-nowrap border-b-2 py-2 px-1 text-base font-medium ${isActive && "text-indigo-600 border-indigo-600"}`}
-                    >
-                        {item.title}
-                    </Link>
-                    )
-                })
-            }
+                    items.map((item, index) => {
+                        const isActive = pathname.startsWith(item.href)
+                        if (item.hide) return <></>
+                        return (
+                        <Link
+                            key={index}
+                            href={item.disabled ? "#" : item.href}
+                            className={`text-gray-900 border-transparent flex-1 whitespace-nowrap border-b-2 mt-2 py-2 px-1 text-base font-medium ${isActive && "text-indigo-600 border-indigo-600 font-bold underline underline-offset-8"}`}
+                        >
+                            {item.title}
+                        </Link>
+                        )
+                    })
+                }
+                <time className={"float-right"}>
+                    {new Date().toLocaleDateString('en-us', {
+                        year: 'numeric',
+                        month: 'short',
+                        day: 'numeric',
+                        weekday: 'short'
+                    })}
+                </time>
             </nav>
+            
         </header>
     )
 }
