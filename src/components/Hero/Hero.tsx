a5c1649aa9528c3416a30f8c0f561e6a339cd49b
eprado99 <a01570318@itesm.mx>
2023-05-16 12:02:26 -0600

    add heroicons package

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,6 +4,8 @@ import * as React from "react"
 
 import Image from 'next/image'
 
+import { AcademicCapIcon } from '@heroicons/react/24/solid'
+
 export default function Hero() {
     return (
       <main className="grid mx-2 md:mx-1 lg:mx-0 md:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
@@ -23,7 +25,7 @@ export default function Hero() {
           <p className="text-lg mt-4 text-slate-600 max-w-xl">
             In here you are able to find all about my latest projects and
             blogs.<wbr /> The main focus of my blogs will be about technology but I
-            might slip in one or two fitness/gym blogs.
+            might slip in one or two fitness/gym blogs. Graduating June 2023 from B.S. in Computer Science & Technology <AcademicCapIcon className="inline-block w-6 h-6 text-slate-500" />
           </p>
         <div className="mt-6 flex flex-col sm:flex-row gap-3">
 
