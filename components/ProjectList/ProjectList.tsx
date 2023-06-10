a670b694638e5cda40be5ee2a47ab0ea29b0a7a4
eprado99 <a01570318@itesm.mx>
2023-06-09 23:36:06 -0600

    change mdx provider

--- components/ProjectList/ProjectList.tsx
+++ components/ProjectList/ProjectList.tsx
@@ -2,15 +2,17 @@
 
 import { MdxProjectMetadata } from "@/types"
 import ProjectCard from "../UI/ProjectCard/ProjectCard"
+import { allProjectPosts } from 'contentlayer/generated'
 
 interface ProjectListProps {
     projectMetadataArr: MdxProjectMetadata[]
 }
 export default function ProjectList({projectMetadataArr}: ProjectListProps) {
+    const posts = allProjectPosts
     return (
         <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6 justify-items-center"}>
-           { projectMetadataArr.map((project) => {
-                return <ProjectCard key={project.slug} project={project}/>
+           { posts.map((post) => {
+                return <ProjectCard key={post.title} {...post}/>
             })}
         </div>
     )
--- components/UI/ProjectCard/ProjectCard.tsx
+++ components/UI/ProjectCard/ProjectCard.tsx
@@ -3,25 +3,26 @@ import * as React from "react"
 import Link from "next/link"
 import Image from "next/image"
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid"
+import { ProjectPost } from "contentlayer/generated"
 interface ProjectListProps {
     project: MdxProjectMetadata
 }
-export default function ProjectCard({project}:ProjectListProps) {
+export default function ProjectCard(projectPost:ProjectPost) {
     return (
         <div className={"block w-80 min-h-[20rem] bg-white rounded-3xl p-4 shadow-2xl cardContainer"}>
             <div className={"cardImageContainer mb-4"}>
                 <Image src="https://picsum.photos/500" alt="Image from picsum" width={288} height={288} className={"text-center rounded-2xl object-fill object-center shadow-2xl overflow-hidden"}/>
             </div>
             <div className={"grid grid-rows-3 h-44"}>
-                <h3 className={"font-bold leading-normal mb-2 w-full"}>{project.title}</h3>
-                <p className={"leading-normal mt-1 min-h-24 w-18 row-span-2 overflow-auto"}>{project.description}</p>
+                <h3 className={"font-bold leading-normal mb-2 w-full"}>{projectPost.title}</h3>
+                <p className={"leading-normal mt-1 min-h-24 w-18 row-span-2 overflow-auto"}>{projectPost.description}</p>
                 <div className={"grid grid-cols-2 mt-4 w-full"}>
                    
-                    <a href={project.githubLink ? project.githubLink : "#"} target="_blank" className={"flex gap-1"} style={{ color: 'inherit', textDecoration: 'none', cursor: 'pointer' }}>
+                    <a href={projectPost.githubLink ? projectPost.githubLink : "#"} target="_blank" className={"flex gap-1"} style={{ color: 'inherit', textDecoration: 'none', cursor: 'pointer' }}>
                         <ArrowTopRightOnSquareIcon className={"w-6 h-6 justify-self-start self-center"} /> <p className={"self-center"}>Github Repo</p>
                     </a>
                     
-                    <Link href={`/project/${project.slug}`} className={"text-center bg-black text-white rounded-2xl py-2 px-4 justify-self-end"}>Read more</Link>
+                    <Link href={`${projectPost.url}`} className={"text-center bg-black text-white rounded-2xl py-2 px-4 justify-self-end"}>Read more</Link>
                 </div>
             </div>
         </div>
