1d4f2181d0fa3529d8cffce6b6ce0085d307f885
Emilio Prado <emiliopradochible@gmail.com>
2023-09-09 22:46:35 -0600

    add graphql client & packages

---
+++ services/client.ts
@@ -0,0 +1,9 @@
+// Apollo GraphQL Client
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: process.env.WP_GRAPHQL_URL,
+    cache: new InMemoryCache(),
+});
+
+export default client;
