30c591e17b0996bc6cdff989cccc00064796b8bf
Emilio Prado <69476395+eprado99@users.noreply.github.com>
2023-09-15 04:26:26 +0000

    added check for face params to support auto detection of faces with cloudinary

--- utils/cloudinaryLoader.ts
+++ utils/cloudinaryLoader.ts
@@ -2,8 +2,13 @@
 // <Image width="..." height="..." src="/myimages/image.jpg" alt="..." />
 const normalizeSrc = (src: string) => src[0] === '/' ? src.slice(1) : src
 
-export default function cloudinaryLoader( src : string , width: number, quality: number | undefined) {
+export default function cloudinaryLoader( src : string , width: number, quality: number | undefined, face: boolean | undefined) {
+    if(face) {
+        const params = ['c_thumb', 'g_face', `w_${width}`, `q_${quality || 'auto'}`]
+        return `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/${params.join(',')}/${normalizeSrc(src)}`;
+    }
     const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`]
     return `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/${params.join(',')}/${normalizeSrc(src)}`;
 }
+
 
