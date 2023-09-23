00cb3517f7af991899406d2d50adfbb8112de26e
Emilio Prado <emiliopradochible@gmail.com>
2023-09-23 17:38:44 -0600

    cleanup horizontal scroll section imports and center cards

--- components/VerticalToHorizontal/HorizontalScrollSection.tsx
+++ components/VerticalToHorizontal/HorizontalScrollSection.tsx
@@ -1,9 +1,6 @@
 "use client"
-import { Node } from "@/types/blogMetadataTypes";
-import BlogCard from "../UI/BlogCard/BlogCard";
 import { motion, useTransform, useScroll } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
-import { getBlogMetadata } from "@/services/blogServices";
+import { useRef } from "react";
 
 const HorizontalScrollSection = ({children,}:{children: React.ReactNode}) => {
     const sectionRef = useRef<HTMLDivElement>(null);
@@ -14,7 +11,7 @@ const HorizontalScrollSection = ({children,}:{children: React.ReactNode}) => {
     return (
         <section ref={sectionRef} className={"expanded-container relative h-[300vh] bg-zinc-950"}>
             <div className={"sticky top-0 h-screen flex items-center overflow-hidden"}>
-                <motion.div style={{ translateX: horizontal }} className={"flex gap-4"}>
+                <motion.div style={{ translateX: horizontal }} className={"flex gap-4 mx-auto"}>
                     { children }
                 </motion.div>
             </div>
