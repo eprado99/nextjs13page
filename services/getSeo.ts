f5e95c14c53fe3c5e73ad7714ccc32264505c1a6
Emilio Prado <emiliopradochible@gmail.com>
2023-09-24 11:06:04 -0600

    add graphql query to retrieve seo metadata

---
+++ services/getSeo.ts
@@ -0,0 +1,33 @@
+import { RootPostSeo } from "@/types/seoMetadataTypes";
+
+const GetPostSeoBySlugQuery = (slug: string): { query: string, variables: { id: string } } => {
+    return {
+        query: `query GetPostSeoBySlug($id: ID = "") {
+            post(id: $id, idType: SLUG) {
+                seo {
+                    metaDesc
+                    title
+                }
+            }
+        }
+        `,
+        variables: {
+            "id": slug
+        },
+    }
+}
+
+export const getPostSeo = async (slug: string) => {
+    const res = await fetch(`${process.env.WP_GRAPHQL_URL}`, {
+        method: "POST",
+        body: JSON.stringify(GetPostSeoBySlugQuery(slug)),
+        headers: {
+          "Content-Type": "application/json",
+        },
+    });
+    const { data }: RootPostSeo = await res.json();
+
+    const seo = data.post.seo;
+
+    return seo;
+}
