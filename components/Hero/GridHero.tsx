804d4c4cf1ade882652f99f45e0fb63740243842
Emilio Prado <69476395+eprado99@users.noreply.github.com>
2023-10-05 18:48:28 +0000

    add inverted corners and arrow icon

--- components/Hero/GridHero.tsx
+++ components/Hero/GridHero.tsx
@@ -3,13 +3,14 @@ import { AcademicCapIcon } from '@heroicons/react/24/solid'
 import Image from 'next/image';
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation"
 import GridHeroMarquees from '../UI/GridHeroMarquees/GridHeroMarquees';
+import Icon from '../UI/Icon/Icon';
 
 const GridHero = () => {
 
     return (
-        <div className="h-[90vh] grid grid-cols-4 md:grid-cols-6 grid-rows-6 gap-3 mt-4 mb-2">
+        <div className="h-[100vh] grid grid-cols-4 md:grid-cols-6 grid-rows-6 gap-3 mt-4 mb-2">
             <div className="border-2 border-black order-1 col-span-4 md:order-none md:col-span-3 row-span-1 md:row-span-2 rounded-lg">
-                <h1 className="text-5xl lg:text-6xl xl:text-8xl font-bold lg:tracking-tight">
+                <h1 className="text-5xl lg:text-6xl xl:text-6xl 2xl:text-8xl font-bold lg:tracking-tight">
                     Welcome to my personal site!
                 </h1>
             </div>
@@ -22,7 +23,7 @@ const GridHero = () => {
                 />
             </div>
             
-            <div className="border-2 border-black text-sm md:text-4xl order-5 md:order-none rounded-lg font-kleeone skew-y-6">She&#39;s playful, loving and a heavy eater.</div>
+            <div className="border-2 border-black text-sm md:text-xl 2xl:text-2xl order-5 md:order-none rounded-lg font-kleeone skew-y-6">She&#39;s playful, loving and a heavy eater.</div>
             <div className="hidden md:block border-2 border-black row-span-3 rounded-lg relative">
                 {/* To make image responsive, pass width as needed to cloudinary, assign relative to parent element, add fill property to Image and style with w-full and h-auto */}
                 <Image 
@@ -32,12 +33,12 @@ const GridHero = () => {
                     alt={'A cat named Mia is walking on the chest of the owner, who is lying down.'} 
                 />
             </div>
-            <div className="border-2 border-black text-sm md:text-4xl order-4 md:order-none rounded-lg font-kleeone -skew-y-6">Her Name is Mia!</div>
+            <div className="border-2 border-black text-sm md:text-xl 2xl:text-2xl order-4 md:order-none rounded-lg font-kleeone -skew-y-6">Her Name is Mia!</div>
             <div className="border-2 border-black order-6 md:order-none rounded-lg relative">                
                 <Image fill src={cloudinaryLoader("portfolio/Mia/lh9ahn7lnpfcvghpv1mm", 240, 100, false)} alt="A cat named Mia lying down with her arms crossed in front of her." className={"w-full h-auto object-cover rounded-lg"}/>
             </div>
             <div className="border-2 border-black order-2 col-span-4 md:col-span-3 md:order-none row-span-2 rounded-lg">
-                <p className="text-sm text-justify md:text-lg lg:text-xl xl:text-2xl mx-auto md:mx-1 mt-4 text-slate-600 max-w-2xl font-kleeone leading-relaxed md:[word-spacing:0.2rem]">
+                <p className="text-sm text-justify md:text-md lg:text-lg xl:text-lg 2xl:text-2xl mx-auto md:mx-1 text-slate-600 max-w-2xl font-kleeone leading-relaxed md:[word-spacing:0.2rem]">
                     <RoughNotationGroup show={true}>
                         <span>I&apos;m Emilio Prado, a </span>
                         <RoughNotation iterations={1} padding={[1, 0, 1, 0]} animationDuration={500} animationDelay={1} type={"highlight"} color={"yellow"}>Software Engineer</RoughNotation>
@@ -91,11 +92,16 @@ const GridHero = () => {
             <div className={"hidden border-2 border-black rounded-lg md:col-span-2 md:flex md:items-center md:justify-center"}>
                 
             </div>
-            <div className="hidden bg-black rounded-lg md:grid md:grid-rows-2 md:grid-cols-4 relative">
-                <div className={"rounded-lg row-start-1 row-end-3 col-start-1 col-end-4 about-info"}>1</div>
-                <div className={"rounded-lg"}>3</div>
-                <div className={"bg-white border-white col-start-4 col-end-5 row-start-1 row-end-2 flex items-center justify-center"} style={{ boxShadow: "0 -0.3rem 0 white, 0.3rem 0 0 white"}}>
-                    <div className={"rounded-full bg-black w-12 h-12"}></div>
+            {/* md:grid md:grid-rows-2 md:grid-cols-4 */}
+            <div className="about hidden bg-black rounded-lg relative">
+                {/* row-start-1 row-end-3 col-start-1 col-end-4 */}
+                <div className={"rounded-lg about-info"}></div>
+                <div className={"rounded-lg about-empty"}>3</div>
+                <div className={"about-button bg-white col-start-4 col-end-5 row-start-1 row-end-2 flex items-center justify-center"} style={{ boxShadow: "0 -0.3rem 0 white, 0.3rem 0 0 white"}}>
+                    <div className={"rounded-full bg-black w-10 h-10 2xl:w-12 2xl:h-12 flex items-center justify-center"}>
+                        {/* @ts-expect-error Server Component */}
+                        <Icon name={"north_east"} width={24} style={{ fill: "white"}} />
+                    </div>
                 </div>
 
             </div>
