4ead7e33856e2b1326fe4e93a915aad4b8a8d8c8
Emilio Prado <emiliopradochible@gmail.com>
2023-10-04 20:19:23 -0600

    start card

--- components/Hero/GridHero.tsx
+++ components/Hero/GridHero.tsx
@@ -8,7 +8,12 @@ const GridHero = () => {
 
     return (
         <div className="h-[90vh] grid grid-cols-4 md:grid-cols-6 grid-rows-6 gap-3 mt-4 mb-2">
-            <div className=" order-3 md:order-none rounded-lg relative">
+            <div className="border-2 border-black order-1 col-span-4 md:order-none md:col-span-3 row-span-1 md:row-span-2 rounded-lg">
+                <h1 className="text-5xl lg:text-6xl xl:text-8xl font-bold lg:tracking-tight">
+                    Welcome to my personal site!
+                </h1>
+            </div>
+            <div className="border-2 border-black order-3 md:order-none rounded-lg relative">
                 <Image 
                     fill 
                     src={cloudinaryLoader("portfolio/Mia/GracefulMia", 240, 100, false)} 
@@ -16,11 +21,7 @@ const GridHero = () => {
                     className={"w-full h-auto object-cover rounded-lg"}
                 />
             </div>
-            <div className=" order-1 col-span-4 md:order-none md:col-span-3 row-span-1 md:row-span-2 rounded-lg">
-                <h1 className="text-5xl lg:text-6xl xl:text-8xl font-bold lg:tracking-tight">
-                    Welcome to my personal site!
-                </h1>
-            </div>
+            
             <div className="border-2 border-black text-sm md:text-4xl order-5 md:order-none rounded-lg font-kleeone skew-y-6">She&#39;s playful, loving and a heavy eater.</div>
             <div className="hidden md:block border-2 border-black row-span-3 rounded-lg relative">
                 {/* To make image responsive, pass width as needed to cloudinary, assign relative to parent element, add fill property to Image and style with w-full and h-auto */}
@@ -31,11 +32,11 @@ const GridHero = () => {
                     alt={'A cat named Mia is walking on the chest of the owner, who is lying down.'} 
                 />
             </div>
-            <div className=" text-sm md:text-4xl order-4 md:order-none rounded-lg font-kleeone -skew-y-6">Her Name is Mia!</div>
-            <div className=" order-6 md:order-none rounded-lg relative">                
+            <div className="border-2 border-black text-sm md:text-4xl order-4 md:order-none rounded-lg font-kleeone -skew-y-6">Her Name is Mia!</div>
+            <div className="border-2 border-black order-6 md:order-none rounded-lg relative">                
                 <Image fill src={cloudinaryLoader("portfolio/Mia/lh9ahn7lnpfcvghpv1mm", 240, 100, false)} alt="A cat named Mia lying down with her arms crossed in front of her." className={"w-full h-auto object-cover rounded-lg"}/>
             </div>
-            <div className=" order-2 col-span-4 md:col-span-3 md:order-none row-span-2 rounded-lg">
+            <div className="border-2 border-black order-2 col-span-4 md:col-span-3 md:order-none row-span-2 rounded-lg">
                 <p className="text-sm text-justify md:text-lg lg:text-xl xl:text-2xl mx-auto md:mx-1 mt-4 text-slate-600 max-w-2xl font-kleeone leading-relaxed md:[word-spacing:0.2rem]">
                     <RoughNotationGroup show={true}>
                         <span>I&apos;m Emilio Prado, a </span>
@@ -56,7 +57,7 @@ const GridHero = () => {
                     </RoughNotationGroup>
                 </p>
             </div>
-            <div className={"hidden md:block  rounded-lg md:col-span-2 row-span-2 relative"}>
+            <div className={"border-2 border-black hidden md:block  rounded-lg md:col-span-2 row-span-2 relative"}>
                 <Image
                     src="/undraw_cat_epte.svg"
                     alt="A black cat"
@@ -65,9 +66,9 @@ const GridHero = () => {
                     className={"w-full h-auto object-cover rounded-lg"}
                 />
             </div>
-            <div className="hidden md:block  rounded-lg">8 link to more about me? (Hidden on Mobile)</div>
+            <div className="hidden md:block border-2 border-black rounded-lg">8 link to more about me? (Hidden on Mobile)</div>
             
-            <div className=" order-7 md:order-none col-span-4 md:col-span-3 rounded-lg flex items-center justify-center">
+            <div className="border-2 border-black order-7 md:order-none col-span-4 md:col-span-3 rounded-lg flex items-center justify-center">
                 <GridHeroMarquees>
                     <li>Web Development</li>
                     <li>CMS</li>
@@ -75,11 +76,11 @@ const GridHero = () => {
                     <li>Service Integration</li>
                 </GridHeroMarquees>
             </div>
-            <div className={"hidden  rounded-lg md:col-span-2 md:flex md:items-center md:justify-center"}>
+            <div className={"hidden border-2 border-black rounded-lg md:col-span-2 md:flex md:items-center md:justify-center"}>
                 
             </div>
-            <div className="hidden md:block  rounded-lg">10 explore adding a real time spotify widget or ig widget? (Hidden on Mobile)</div>
-            <div className=" order-8 md:order-none col-span-4 md:col-span-3 rounded-lg flex items-center justify-center">
+            <div className="hidden md:block border-2 border-black rounded-lg">10 explore adding a real time spotify widget or ig widget? (Hidden on Mobile)</div>
+            <div className="border-2 border-black order-8 md:order-none col-span-4 md:col-span-3 rounded-lg flex items-center justify-center">
                 <GridHeroMarquees>
                     <li>Web Development</li>
                     <li>CMS</li>
@@ -87,10 +88,17 @@ const GridHero = () => {
                     <li>Service Integration</li>
                 </GridHeroMarquees>
             </div>
-            <div className={"hidden  rounded-lg md:col-span-2 md:flex md:items-center md:justify-center"}>
+            <div className={"hidden border-2 border-black rounded-lg md:col-span-2 md:flex md:items-center md:justify-center"}>
                 
             </div>
-            <div className="hidden md:block  rounded-lg">8 link to more about me? (Hidden on Mobile)</div>
+            <div className="hidden border-2 border-black bg-black rounded-lg md:grid md:grid-rows-2 md:grid-cols-4">
+                <div className={"rounded-lg row-start-1 row-end-3 col-start-1 col-end-4"}>1</div>
+                <div className={"rounded-lg"}>3</div>
+                <div className={"rounded-lg bg-white col-start-4 col-end-5 row-start-1 row-end-2 flex items-center justify-center"}>
+                    <div className={"rounded-full bg-black w-12 h-12"}></div>
+                </div>
+
+            </div>
 
             </div>
             
