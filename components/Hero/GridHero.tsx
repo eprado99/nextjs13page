dc70063c06bee3b2269db1a088b1fb651d38d914
Emilio Prado <emiliopradochible@gmail.com>
2023-10-07 22:14:06 -0600

    updates to grid hero

--- components/Hero/GridHero.tsx
+++ components/Hero/GridHero.tsx
@@ -7,8 +7,11 @@ import Icon from '../UI/Icon/Icon';
 
 const GridHero = () => {
 
+    {/* To make image responsive, pass width as needed to cloudinary, assign relative to parent element, add fill property to Image and style with w-full and h-auto */}
+
+
     return (
-        <div className="h-[100vh] grid grid-cols-4 md:grid-cols-6 grid-rows-6 gap-3 mt-4 mb-2">
+        <div className="h-[100vh] grid grid-cols-4 md:grid-cols-5 grid-rows-6 gap-3 mt-4 mb-2">
             <div className="border-2 border-black order-1 col-span-4 md:order-none md:col-span-3 row-span-1 md:row-span-2 rounded-lg">
                 <h1 className="text-5xl lg:text-6xl xl:text-6xl 2xl:text-8xl font-bold lg:tracking-tight">
                     Welcome to my personal site!
@@ -24,15 +27,14 @@ const GridHero = () => {
             </div>
             
             <div className="border-2 border-black text-sm md:text-xl 2xl:text-2xl order-5 md:order-none rounded-lg font-kleeone skew-y-6">She&#39;s playful, loving and a heavy eater.</div>
-            <div className="hidden md:block border-2 border-black row-span-3 rounded-lg relative">
-                {/* To make image responsive, pass width as needed to cloudinary, assign relative to parent element, add fill property to Image and style with w-full and h-auto */}
+            {/* <div className="hidden md:block border-2 border-black row-span-3 rounded-lg relative">
                 <Image 
                     fill
                     src={cloudinaryLoader("portfolio/Mia/MiaWalking", 240, 100, false)} 
                     className={"w-full h-auto rounded-lg"}
                     alt={'A cat named Mia is walking on the chest of the owner, who is lying down.'} 
                 />
-            </div>
+            </div> */}
             <div className="border-2 border-black text-sm md:text-xl 2xl:text-2xl order-4 md:order-none rounded-lg font-kleeone -skew-y-6">Her Name is Mia!</div>
             <div className="border-2 border-black order-6 md:order-none rounded-lg relative">                
                 <Image fill src={cloudinaryLoader("portfolio/Mia/lh9ahn7lnpfcvghpv1mm", 240, 100, false)} alt="A cat named Mia lying down with her arms crossed in front of her." className={"w-full h-auto object-cover rounded-lg"}/>
@@ -67,7 +69,7 @@ const GridHero = () => {
                     className={"w-full h-auto object-cover rounded-lg"}
                 />
             </div>
-            <div className="hidden md:block border-2 border-black rounded-lg">8 link to more about me? (Hidden on Mobile)</div>
+            {/* <div className="hidden md:block border-2 border-black rounded-lg">8 link to more about me? (Hidden on Mobile)</div> */}
             
             <div className="border-2 border-black order-7 md:order-none col-span-4 md:col-span-3 rounded-lg flex items-center justify-center">
                 <GridHeroMarquees>
@@ -77,10 +79,29 @@ const GridHero = () => {
                     <li>Service Integration</li>
                 </GridHeroMarquees>
             </div>
-            <div className={"hidden border-2 border-black rounded-lg md:col-span-2 md:flex md:items-center md:justify-center"}>
-                
+            <div className="border-2 border-black order-9 md:order-none row-span-2 rounded-lg relative">
+                {/* To make image responsive, pass width as needed to cloudinary, assign relative to parent element, add fill property to Image and style with w-full and h-auto */}
+                <Image 
+                    fill
+                    src={cloudinaryLoader("portfolio/Mia/MiaWalking", 240, 100, false)} 
+                    className={"w-full h-auto rounded-lg"}
+                    alt={'A cat named Mia is walking on the chest of the owner, who is lying down.'} 
+                />
             </div>
-            <div className="hidden md:block border-2 border-black rounded-lg">10 explore adding a real time spotify widget or ig widget? (Hidden on Mobile)</div>
+
+            {/* md:grid md:grid-rows-2 md:grid-cols-4 */}
+            <div className="hidden order-10 md:order-none bg-black rounded-lg relative">
+                {/* row-start-1 row-end-3 col-start-1 col-end-4 */}
+                <div className={"rounded-lg about-info text-white m-2"}>Learn more about me!</div>
+                <div className={"rounded-lg about-empty"}></div>
+                <div className={"about-button bg-white col-start-4 col-end-5 row-start-1 row-end-2 flex items-center justify-center"} style={{ boxShadow: "0 -0.3rem 0 white, 0.3rem 0 0 white"}}>
+                    <div className={"rounded-full bg-black w-10 h-10 2xl:w-12 2xl:h-12 flex items-center justify-center"}>
+                        {/* @ts-expect-error Server Component */}
+                        <Icon name={"north_east"} width={24} style={{ fill: "white"}} />
+                    </div>
+                </div>
+            </div>
+
             <div className="border-2 border-black order-8 md:order-none col-span-4 md:col-span-3 rounded-lg flex items-center justify-center">
                 <GridHeroMarquees>
                     <li>Web Development</li>
@@ -89,11 +110,9 @@ const GridHero = () => {
                     <li>Service Integration</li>
                 </GridHeroMarquees>
             </div>
-            <div className={"hidden border-2 border-black rounded-lg md:col-span-2 md:flex md:items-center md:justify-center"}>
-                
-            </div>
+            
             {/* md:grid md:grid-rows-2 md:grid-cols-4 */}
-            <div className="about hidden bg-black rounded-lg relative">
+            <div className="about col-span-2 md:col-span-1 order-11 md:order-none bg-black rounded-lg relative">
                 {/* row-start-1 row-end-3 col-start-1 col-end-4 */}
                 <div className={"rounded-lg about-info text-white m-2"}>Learn more about me!</div>
                 <div className={"rounded-lg about-empty"}></div>
@@ -106,7 +125,7 @@ const GridHero = () => {
 
             </div>
 
-            </div>
+        </div>
             
     )
 }
