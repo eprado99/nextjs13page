980a87c2eb3a5e25a206dc4a947c03023ad89f17
Emilio Prado <emiliopradochible@gmail.com>
2023-10-10 20:52:08 -0600

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
 import Hero from '@/components/Hero/Hero'
 import ScrollingTechCards from '@/components/Technologies/ScrollingTechCards'
-import Timeline from '@/components/Timeline/Timeline'
+// import Timeline from '@/components/Timeline/Timeline'
 import HorizontalScrollSection from '@/components/VerticalToHorizontal/HorizontalScrollSection'
 import Footer from '@/components/Footer/Footer'
 import { workExperienceData } from '@/config/workExperience'
 import { programmingLanguagesSvg, frameworksSvg, cloudSvg } from '@/config/svgItems'
-import BlogList from '@/components/ServerComponents/BlogList'
+// import BlogList from '@/components/ServerComponents/BlogList'
 import HorizontalTimeline from '@/components/Timeline/HorizontalTimeline'
-import GridHero from '@/components/Hero/GridHero'
+// import GridHero from '@/components/Hero/GridHero'
+import ProjectList from '@/components/ServerComponents/ProjectList'
 
 export default async function Home() {
   return (
     <>
-      <GridHero />
+      {/* <GridHero /> */}
       <Hero />
       <HorizontalTimeline work={workExperienceData.workExperienceArr}/>
       <ScrollingTechCards programmingSvgItems={programmingLanguagesSvg.svgItemArray} frameworkSvgItems={frameworksSvg.svgItemArray} cloudSvgItems={cloudSvg.svgItemArray}/>
       <HorizontalScrollSection>
         {/* @ts-expect-error Server Component */}
-        {<BlogList />}
+        {<ProjectList />}
       </HorizontalScrollSection>
       <Footer />
     </>
