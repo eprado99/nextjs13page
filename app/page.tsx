adaaf371e29eedc39f9ed7ba77384987b493c3ae
Emilio Prado <emiliopradochible@gmail.com>
2023-10-15 19:54:22 -0600

    removed footer

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
       {/* </HorizontalScrollSection> */}
       {/* @ts-expect-error Server Component */}
       <ProjectList classNames={'expanded-container bg-black h-[100vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6 justify-center items-center px-10'}/>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
